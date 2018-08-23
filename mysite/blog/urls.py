fcd4e9959bd0477a3b09c67170c4d2230d6ba397
veraeikon <mamusinkowo@gmail.com>
2018-08-23 23:40:05 +0200

    helpunku

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import PostListView
+from blog.views import PostListView, PostView
 
 urlpatterns = [
         path('', PostListView.as_view(), name='post_list'),
+        path('posts/<int:pk>/', PostView.as_view(), name='post_view'),
         ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from blog.models import Post
 
@@ -11,5 +12,13 @@ class PostListView(ListView):
         context = super().get_context_data(**kwargs)
         context['now']=timezone.now()
         return context
+
+class PostView(DetailView):
     
+    model = Post
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now']=timezone.now()
+        return context
 
